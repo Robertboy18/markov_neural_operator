8edad7a1121dc91bff7dfab214dafec95210d018
Robert Joseph <robujsph2001@gmail.com>
2023-09-19 16:52:22 -0700

    done

--- scripts/NS_fno_baseline-trial.py
+++ scripts/NS_fno_baseline-trial.py
@@ -31,7 +31,7 @@ wandb.login(key='0d28fab247b1d30084a6ea7af891401bb5d1c20e')
 wandb.init(
     entity='research-pino_ifno',
     project='re5000',
-    name='baseline64-32'
+    name='baseline64-64'
 )
 # Main
 ntrain = 90
