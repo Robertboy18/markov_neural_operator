db5e90e9ebf696051ced3017deb52457c68cd1f9
Robert Joseph <robujsph2001@gmail.com>
2023-10-12 17:58:24 -0700

    done

--- scripts/NS_fno_baseline-trial.py
+++ scripts/NS_fno_baseline-trial.py
@@ -34,7 +34,7 @@ wandb.login(key='0d28fab247b1d30084a6ea7af891401bb5d1c20e')
 wandb.init(
     entity='research-pino_ifno',
     project='re5000',
-    name='incremental-resolution-final-run-20epoch'
+    name='incremental-resolution-final-run-10epoch-bagmix'
 )
 
 # Create an ArgumentParser object
