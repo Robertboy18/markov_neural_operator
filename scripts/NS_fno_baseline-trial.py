e3ff1aad500ab92fe8ac5029f7f21376c782c9fc
Robert Joseph <robujsph2001@gmail.com>
2023-10-23 20:39:44 -0700

    done

--- scripts/NS_fno_baseline-trial.py
+++ scripts/NS_fno_baseline-trial.py
@@ -34,7 +34,7 @@ wandb.login(key='0d28fab247b1d30084a6ea7af891401bb5d1c20e')
 wandb.init(
     entity='research-pino_ifno',
     project='re5000',
-    name='joint-incremental-lossgap-final-run-mixscheduler-50epochs'
+    name='incremental-resolution-final-run-steplr'
 )
 
 # Create an ArgumentParser object
@@ -158,13 +158,13 @@ trainer = Trainer(model = model, n_epochs=epochs,
                   log_output=config.wandb.log_output,
                   use_distributed=config.distributed.use_distributed,
                   verbose=config.verbose, incremental = False, 
-                  incremental_loss_gap=True, 
+                  incremental_loss_gap=False, 
                   incremental_resolution=True, dataset_name="Re5000", save_interval=config.checkpoint.interval, model_save_dir=config.checkpoint.directory + config.checkpoint.name)
 
 
 trainer.train(train_loader=train_loader, test_loaders=test_loader, regularizer=None,
               optimizer=optimizer,
-              scheduler=[scheduler1, scheduler],
+              scheduler=[scheduler1, scheduler1],
               training_loss=train_loss,
               eval_losses=eval_losses)
 
